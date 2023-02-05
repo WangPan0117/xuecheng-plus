769453575c4aa53a9fe492b71d585d01cdcbba84
Wang Pan <1148691033@qq.com>
2023-02-05 22:04:53 +0800

    内容管理模块-课程分类查询

--- xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/content/XuechengPlusContentServiceApplicationTests.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/content/XuechengPlusContentServiceApplicationTests.java
@@ -1,11 +1,15 @@
 package com.xuecheng.content;
 
 import com.xuecheng.content.mapper.CourseBaseMapper;
+import com.xuecheng.content.model.dto.CourseCategoryTreeDto;
 import com.xuecheng.content.model.po.CourseBase;
+import com.xuecheng.content.service.CourseCategoryService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class XuechengPlusContentServiceApplicationTests {
@@ -13,6 +17,8 @@ class XuechengPlusContentServiceApplicationTests {
     @Autowired
     CourseBaseMapper courseBaseMapper;
 
+    @Autowired
+    CourseCategoryService courseCategoryService;
     @Test
     void testCourseBaseMapper() {
         CourseBase courseBase = courseBaseMapper.selectById(1L);
@@ -20,5 +26,9 @@ class XuechengPlusContentServiceApplicationTests {
         System.out.println(courseBase.getAuditStatus());
     }
 
-
+    @Test
+    void testCourseCategoryService() {
+        List<CourseCategoryTreeDto> courseCategoryTreeDtos = courseCategoryService.queryTreeNodes("1");
+        System.out.println(courseCategoryTreeDtos);
+    }
 }
