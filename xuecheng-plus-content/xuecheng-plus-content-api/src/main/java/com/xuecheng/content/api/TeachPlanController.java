9863d9ecada2e1cbd7eddea3d97e179a366a8754
WangPan <1148691033@qq.com>
2023-02-28 16:37:42 +0800

    内容管理模块-新增修改课程计划

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/TeachPlanController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/TeachPlanController.java
@@ -1,14 +1,13 @@
 package com.xuecheng.content.api;
 
+import com.xuecheng.content.model.dto.SaveTeachplanDto;
 import com.xuecheng.content.model.dto.TeachPlanDto;
 import com.xuecheng.content.service.TeachplanService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -26,5 +25,11 @@ public class TeachPlanController {
     @GetMapping("/teachplan/{courseId}/tree-nodes")
     public List<TeachPlanDto> getTeachPlanTreeNodes(@PathVariable Long courseId){
         return teachplanService.findTeachplayTree(courseId);
+    }
+
+    @ApiOperation("课程计划创建或修改")
+    @PostMapping("/teachplan")
+    public void saveTeachplan( @RequestBody SaveTeachplanDto teachplanDto){
+        teachplanService.saveTeachplan(teachplanDto);
     }
 }
